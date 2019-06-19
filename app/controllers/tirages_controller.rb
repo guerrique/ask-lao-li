54b26f4d8749483a4686876361c38a6c5a7d4e64
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-06-19 14:59:25 +0200

    styling lancer

--- app/controllers/tirages_controller.rb
+++ app/controllers/tirages_controller.rb
@@ -1,4 +1,5 @@
 class TiragesController < ApplicationController
+  before_action :set_tirage, only: [:edit]
   def new
     @tirage = Tirage.new
   end
@@ -7,15 +8,22 @@ class TiragesController < ApplicationController
     @tirage = Tirage.new(tirage_params)
     @tirage.user = current_user
     if @tirage.save
-      redirect_to "/"
+      redirect_to edit_tirage_path(@tirage)
     else
       render :new
     end
+  end
+
+  def edit
   end
 
   private
 
   def tirage_params
     params.require(:tirage).permit(:question)
+  end
+
+  def set_tirage
+    @tirage = Tirage.find(params[:id])
   end
 end
