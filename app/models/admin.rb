ffad3fd5339367cdac42d44a231d051d7664e2b0
guerrique desmoulins guerrique@gmail.com <guerrique@gmail.com>
2019-06-22 17:51:37 +0200

    tried to add admin

---
+++ app/models/admin.rb
@@ -0,0 +1,10 @@
+class Admin < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable
+
+
+  def admin?
+    true
+  end
+end
--- db/migrate/20190616194705_create_hexagrammes.rb
+++ db/migrate/20190616194705_create_hexagrammes.rb
@@ -1,20 +1,31 @@
 class CreateHexagrammes < ActiveRecord::Migration[5.2]
   def change
     create_table :hexagrammes do |t|
-      t.string :nom
-      t.string :description
-      t.string :trait1
-      t.string :trait2
-      t.string :trait3
-      t.string :trait4
-      t.string :trait5
-      t.string :trait6
-      t.string :image
       t.integer :numero
+      t.string :nom
+      t.text :description
+      t.text :jugement
+      t.text :jugement_description
+      t.text :trait1
+      t.text :trait1_description
+      t.text :trait2
+      t.text :trait2_description
+      t.text :trait3
+      t.text :trait3_description
+      t.text :trait4
+      t.text :trait4_description
+      t.text :trait5
+      t.text :trait5_description
+      t.text :trait6
+      t.text :trait6_description
+      t.text :grande_image
+      t.text :dixieme_aile
+      t.text :nucleaire
+      t.text :oppose
+      t.text :renverse
+      t.string :image
       t.string :trigramme_haut
       t.string :trigramme_bas
-      t.string :oppose
-      t.string :reverse
 
       t.timestamps
     end
